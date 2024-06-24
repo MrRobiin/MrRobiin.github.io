47d84446f7fbcd65b17d26975bf5dd0a318d5f49
Robin <robin.lucas7@gmail.com>
2024-06-24 12:03:12 +0200

    updated the page
    added buttons, some text and light and dark mode

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+// script.js
 
 const image = document.getElementById('image');
+const faviconDiv = document.getElementById('favicon-div');
+const faviconImg = document.getElementById('favicon-img');
+const favicon = document.getElementById('favicon');
 
 image.addEventListener('dblclick', () => {
     image.classList.add('spin');
     setTimeout(() => {
         image.classList.remove('spin');
     }, 1000); // Tiden ska matcha varaktigheten för CSS-animationen
+});
+
+let isDarkMode = false;
+
+faviconDiv.addEventListener('click', () => {
+    if (isDarkMode) {
+        document.body.classList.remove('dark-mode');
+        faviconImg.src = 'img/sun.png';
+        favicon.href = 'sun.ico';
+    } else {
+        document.body.classList.add('dark-mode');
+        faviconImg.src = 'img/moon.png';
+        favicon.href = 'moon.ico';
+    }
+    isDarkMode = !isDarkMode;
 });
