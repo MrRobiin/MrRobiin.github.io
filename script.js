bcde6f693559aac6f13917ad04837776a23138b4
Robin <robin.lucas7@gmail.com>
2024-06-25 13:09:53 +0200

    still button fixes
    Added a cookie button

--- script.js
+++ script.js
@@ -65,3 +65,41 @@ faviconDiv.addEventListener('click', () => {
               }
             }
           }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (!getCookie('cookiesAccepted')) {
+      document.getElementById('cookieBanner').style.display = 'block';
+  }
+
+  document.getElementById('acceptCookies').addEventListener('click', function() {
+      setCookie('cookiesAccepted', 'true', 365);
+      document.getElementById('cookieBanner').style.display = 'none';
+  });
+});
+
+// Cookie functions
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
+}
