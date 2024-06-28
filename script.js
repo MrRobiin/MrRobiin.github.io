16f3926a9f67eb6765153ae53fb6f1ab76a4d26c
Robin <robin.lucas7@gmail.com>
2024-06-28 22:00:36 +0200

    bubbles

--- script.js
+++ script.js
@@ -103,3 +103,18 @@ function getCookie(name) {
 function eraseCookie(name) {
   document.cookie = name + '=; Max-Age=-99999999;';
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
